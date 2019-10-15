4d1943a3e76ae3a456c676249df745528574737a
EthanHogan <ethan.hogan7@gmail.com>
2019-10-15 16:32:09 -0500

    added database connection and query to get all info from database

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const path = require('path');
+
 const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/dist');
 module.exports = {
   entry: `${SRC_DIR}/index.js`,
   output: {
-  filename: 'bundle.js',
-  path: DIST_DIR
-},
-  module : {
-    rules : [
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
       {
-        test : /\.js?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',
-      }
-    ]
-  }
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
